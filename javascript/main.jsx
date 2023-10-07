21a2ff1d6798d022e9e6b8f5d9f07cd4b01ee512
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-10-07 12:44:15 -0300

    Creando cursor con forma de estrella

--- javascript/main.jsx
+++ javascript/main.jsx
@@ -4,16 +4,18 @@ document.addEventListener('mousemove', (e) => {
     const star = document.createElement('div');
     star.classList.add('star');
 
-    const size = Math.random() * 4 + 1;
+    const size = Math.random() * 20 + 1;
+    const x = e.pageX - size / 2;
+    const y = e.pageY - size / 2;
+
     star.style.width = `${size}px`;
     star.style.height = `${size}px`;
-
-    star.style.left = `${e.pageX}px`;
-    star.style.top = `${e.pageY}px`;
+    star.style.left = `${x}px`;
+    star.style.top = `${y}px`;
 
     container.appendChild(star);
 
     setTimeout(() => {
         star.remove();
-    }, 300);
+    }, 500);
 })
