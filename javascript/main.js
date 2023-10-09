b4ef9be74bc3a13d2b17fe851bd07e4d23bff75f
Maira Julian <mairabetianajulianbarros@gmail.com>
2023-10-09 19:48:21 -0300

    agregue apps

---
+++ javascript/main.js
@@ -0,0 +1,29 @@
+const container = document.getElementById('headerGrid')
+
+document.addEventListener('mousemove', (e) => {
+    const star = document.createElement('div')
+    star.classList.add('star')
+
+    const size = Math.random() * 20 + 1
+    const x = e.pageX - size / 2
+    const y = e.pageY - size / 2
+
+    star.style.width = `${size}px`
+    star.style.height = `${size}px`
+    star.style.left = `${x}px`
+    star.style.top = `${y}px`
+
+    container.appendChild(star)
+
+    setTimeout(() => {
+        star.remove();
+    }, 200);
+})
+
+document.getElementById('button1').addEventListener('click', function (e) {
+    e.preventDefault()
+    console.log('haciendo click');
+    const proyectosSection = document.getElementById('sectionTitle')
+    proyectosSection.scrollIntoView({ behavior: 'smooth' })
+    // proyectosSection.scrollIntoView({ behavior: 'smooth' })
+})
